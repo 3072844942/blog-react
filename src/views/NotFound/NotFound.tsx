b6595695fef79279fc39fb72e1cf3cf00aab8094
3072844942 <3072844942@qq.com>
2022-06-02 17:34:55 +0800

    q

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import Style from './NotFound.module.scss'
 
 function NotFound() {
     return (
-        <div>
+        <div className={Style.boxNotFound}>
             未找到
         </div>
     );
